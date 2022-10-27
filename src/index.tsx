7ba4114083f9ab4dde45b4e0b97359aa764e3c22
sadthu <dinhhai4443211@gmail.com>
2022-10-27 12:40:49 +0700

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {store} from './redux/configStore'
+import { store } from './redux/configStore'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
