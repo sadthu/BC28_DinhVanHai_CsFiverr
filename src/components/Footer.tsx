428bf50157fe85b3aa2fa9b0afe2e80e8a4766a7
Hải Đinh <dinhhai444321@gmail.com>
2022-10-30 10:29:42 +0700

    add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,75 +1,199 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
     <div>
-      <div className='container d-flex'>
+      <div className="container d-flex justify-content-between">
         <ul>
-            <li><a href="#">Categorles</a></li>
-            <li><a href="#">Graphics & Design</a></li>
-            <li><a href="#">Digital Marketing</a></li>
-            <li><a href="#">Writing & Translation</a></li>
-            <li><a href="#">Video & Animations</a></li>
-            <li><a href="#">Music & Audio</a></li>
-            <li><a href="#">Programming & Tech</a></li>
-            <li><a href="#">Data</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Lifestyle</a></li>
-            <li><a href="#">Sitemap</a></li>
+          <li>
+            <a href="#">Categorles</a>
+          </li>
+          <li>
+            <a href="#">Graphics & Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="#">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="#">Video & Animations</a>
+          </li>
+          <li>
+            <a href="#">Music & Audio</a>
+          </li>
+          <li>
+            <a href="#">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="#">Data</a>
+          </li>
+          <li>
+            <a href="#">Business</a>
+          </li>
+          <li>
+            <a href="#">Lifestyle</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
         </ul>
         <ul>
-            <li><a href="#">Graphics & Design</a></li>
-            <li><a href="#">Digital Marketing</a></li>
-            <li><a href="#">Writing & Translation</a></li>
-            <li><a href="#">Video & Animations</a></li>
-            <li><a href="#">Music & Audio</a></li>
-            <li><a href="#">Programming & Tech</a></li>
-            <li><a href="#">Data</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Lifestyle</a></li>
-            <li><a href="#">Sitemap</a></li>
+          <li>
+            <a href="#">Graphics & Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="#">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="#">Video & Animations</a>
+          </li>
+          <li>
+            <a href="#">Music & Audio</a>
+          </li>
+          <li>
+            <a href="#">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="#">Data</a>
+          </li>
+          <li>
+            <a href="#">Business</a>
+          </li>
+          <li>
+            <a href="#">Lifestyle</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
         </ul>
         <ul>
-            <li><a href="#">Graphics & Design</a></li>
-            <li><a href="#">Digital Marketing</a></li>
-            <li><a href="#">Writing & Translation</a></li>
-            <li><a href="#">Video & Animations</a></li>
-            <li><a href="#">Music & Audio</a></li>
-            <li><a href="#">Programming & Tech</a></li>
-            <li><a href="#">Data</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Lifestyle</a></li>
-            <li><a href="#">Sitemap</a></li>
+          <li>
+            <a href="#">Graphics & Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="#">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="#">Video & Animations</a>
+          </li>
+          <li>
+            <a href="#">Music & Audio</a>
+          </li>
+          <li>
+            <a href="#">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="#">Data</a>
+          </li>
+          <li>
+            <a href="#">Business</a>
+          </li>
+          <li>
+            <a href="#">Lifestyle</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
         </ul>
         <ul>
-            <li><a href="#">Graphics & Design</a></li>
-            <li><a href="#">Digital Marketing</a></li>
-            <li><a href="#">Writing & Translation</a></li>
-            <li><a href="#">Video & Animations</a></li>
-            <li><a href="#">Music & Audio</a></li>
-            <li><a href="#">Programming & Tech</a></li>
-            <li><a href="#">Data</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Lifestyle</a></li>
-            <li><a href="#">Sitemap</a></li>
+          <li>
+            <a href="#">Graphics & Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="#">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="#">Video & Animations</a>
+          </li>
+          <li>
+            <a href="#">Music & Audio</a>
+          </li>
+          <li>
+            <a href="#">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="#">Data</a>
+          </li>
+          <li>
+            <a href="#">Business</a>
+          </li>
+          <li>
+            <a href="#">Lifestyle</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
         </ul>
         <ul>
-            <li><a href="#">Graphics & Design</a></li>
-            <li><a href="#">Digital Marketing</a></li>
-            <li><a href="#">Writing & Translation</a></li>
-            <li><a href="#">Video & Animations</a></li>
-            <li><a href="#">Music & Audio</a></li>
-            <li><a href="#">Programming & Tech</a></li>
-            <li><a href="#">Data</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Lifestyle</a></li>
-            <li><a href="#">Sitemap</a></li>
-        </ul> 
+          <li>
+            <a href="#">Graphics & Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Marketing</a>
+          </li>
+          <li>
+            <a href="#">Writing & Translation</a>
+          </li>
+          <li>
+            <a href="#">Video & Animations</a>
+          </li>
+          <li>
+            <a href="#">Music & Audio</a>
+          </li>
+          <li>
+            <a href="#">Programming & Tech</a>
+          </li>
+          <li>
+            <a href="#">Data</a>
+          </li>
+          <li>
+            <a href="#">Business</a>
+          </li>
+          <li>
+            <a href="#">Lifestyle</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
+        </ul>
       </div>
-      <hr />
-      <div>gggfdgggdfd</div>
+      <hr className="container" />
+      <div className="footer">
+        <div className="container">
+          <div className="footer__content d-flex justify-content-between">
+            <div className="cpright__left">
+              <p>
+                Copyright © 2021
+                <span>DexignZone </span>. All rights reserved.
+              </p>
+            </div>
+            <div className="cpright__right">
+              <a href="#">
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram" />
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
