f6e83629df6a457b91095ee2b25ce36e097398e0
sadthu <dinhhai4443211@gmail.com>
2022-11-01 19:02:42 +0700

    add

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,61 +9,58 @@ export default function Home({ }: Props) {
   const imgNetfix = require('../../assets/img/netfix.png')
   const imgPG = require('../../assets/img/p&g.png')
   const imgPP = require('../../assets/img/paypal.png')
+ 
   return (
     <div>
       <div>
         <header className='w-100'>
           <nav className="navbar navbar-expand-sm navbar-light justify-content-between container">
-            <div className='d-flex'>
-              <a className="navbar-brand text-white" href="#">Fiverr</a>
-              <form className="d-flex my-2 my-lg-0">
-                <input className="form-control me-sm-2" type="text" placeholder="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <div className='d-flex logo'>
+              <a className="navbar-brand text-white fw-bolder fs-3" href="#">Fiverr</a>
+              <form className=" my-2">
+                <input className="form-control" type="text" placeholder="Search"></input>
+                <button className="btn btn-outline-success" type="submit">Search</button>
               </form>
             </div>
-            <div>
+            <div className='form'>
               <ul className="navbar-nav me-auto mt-2 mt-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link text-white" href="#">Become a Seller</a>
+                  <a className=" text-white genaral" href="#">Become a Seller</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link text-white" href="#">Sign in</a>
+                  <a className=" text-white genaral" href="#">Sign in</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link text-white" href="#">Join</a>
+                  <a className=" text-white rounded" href="#">Join</a>
                 </li>
               </ul>
             </div>
           </nav>
         </header>
         <section className='carousell'>
-          <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel slide carousel-fade" data-bs-interval="500">
-              <div className="carousel-item active" data-bs-pause="false">
-                <img src="https://kynguyenlamdep.com/wp-content/uploads/2020/01/hinh-anh-dep-hoa-bo-cong-anh.jpg" className="d-block w-100" alt="..." />
-              </div>
-              <div className="carousel-item" data-bs-pause="false">
-                <img src="https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg" className="d-block w-100" alt="..." />
-              </div>
-              <div className="carousel-item" data-bs-pause="false">
-                <img src="https://kynguyenlamdep.com/wp-content/uploads/2020/01/bo-cong-anh.jpg" className="d-block w-100" alt="..." />
-              </div>
+          <div id="carouselExampleControls" className="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-pause="false">
+            <div className="carousel-inner" >
+              <div className="carousel-item active" data-bs-interval="3000"></div>
+              <div className="carousel-item" data-bs-interval="3000"></div>
+              <div className="carousel-item" data-bs-interval="3000"></div>
+              <div className="carousel-item" data-bs-interval="3000"></div>
+              <div className="carousel-item" data-bs-interval="3000"></div>
             </div>
           </div>
           <div className='carousell__content w-100'>
             <div className='container'>
               <div className='w-50'>
                 <h2>Find the perfect freelance <br /> services for your business</h2>
-                <form className="d-flex my-2 my-lg-0">
-                  <input className="form-control me-sm-2 w-40" type="text" placeholder="Search" />
-                  <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <form className="d-flex my-2">
+                  <input className="form-control" type="text" placeholder="Search" />
+                  <button className="btn" type="submit">Search</button>
                 </form>
                 <p>
                   Popular:
                   <span>Website Design</span>
-                  <span>Website Design</span>
-                  <span>Website Design</span>
-                  <span>Website Design</span>
+                  <span>WordPress</span>
+                  <span>Logo Design</span>
+                  <span>Video Editting</span>
                 </p>
               </div>
             </div>
